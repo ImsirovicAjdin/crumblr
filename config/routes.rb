6f2b86a32360c3f1cb2c0f08f066d7be713ac0e6
ajdin <ajdika@live.com>
2016-09-14 06:29:21 +0000

    add welcome as start page, add /about alias in pages_ctrl, add nav with 2 links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about' => 'pages#about' #hash rocket
+  
+  root 'pages#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
