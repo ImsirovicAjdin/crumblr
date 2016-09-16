7085542a31e8470de2cbf29c9078a2c73697a313
ajdin <ajdika@live.com>
2016-09-16 10:26:46 +0000

    Add support for images

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914093756) do
+ActiveRecord::Schema.define(version: 20160916100901) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
